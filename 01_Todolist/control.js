d27ef0719a297b7f4cf4d2771391c2a01386e61a
rf_104 <kingaref13@gmail.com>
2025-01-25 00:22:14 +0600

    add part modified

--- 01_Todolist/control.js
+++ 01_Todolist/control.js
@@ -2,9 +2,20 @@ const btn = document.querySelector('#add')
 
 btn.addEventListener('click',(e)=>{
     const task = document.getElementById('taskIn').value;
-    const st = document.getElementById('class')
+    const st = document.getElementById('store')
     // st.innerHTML = `<h1>${task}</h1>`
-    const li = document.createElement('li')
-    li.appendChild(document.createTextNode(task))
-    st.appendChild(li)
+    // const li = document.createElement('li')
+    // li.appendChild(document.createTextNode(task))
+    // st.appendChild(li)
+    if(task!=""){
+        
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(task))
+    div.style.backgroundColor = 'gray';
+    div.style.borderRadius = '15px';
+    div.style.padding = '12px'
+    div.style.marginTop = '12px'
+    st.appendChild(div)
+
+    }
 })
