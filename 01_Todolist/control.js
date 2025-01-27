6409bfd9b886d4eb2e6b2fdf96701b5c1cc5070d
rf_104 <kingaref13@gmail.com>
2025-01-27 22:53:27 +0600

    done button perfectly done

--- 01_Todolist/control.js
+++ 01_Todolist/control.js
@@ -1,28 +1,58 @@
 const btn = document.querySelector('#add')
-
+let record = [];
+let flg = false;
 btn.addEventListener('click',(e)=>{
-    const task = document.getElementById('taskIn').value;
+    const task = document.getElementById('taskIn').value.trim();
     const st = document.getElementById('store')
     // st.innerHTML = `<h1>${task}</h1>`
     // const li = document.createElement('li')
     // li.appendChild(document.createTextNode(task))
     // st.appendChild(li)
+
     if(task!=""){
-    
-    const btnCreat = document.createElement('button');
-    btnCreat.textContent = "Done";
+        flg = true;
+        record.push(task)
+        console.log(record);
+        const btnDiv = document.createElement('div');
+        btnDiv.setAttribute('id','btnDiv');
 
 
-    const div = document.createElement('div');
-    div.appendChild(document.createTextNode(task))
-    div.appendChild(btnCreat);
-    div.setAttribute('id','taskDiv');
-    div.style.display = "flex"
-    div.style.backgroundColor = 'gray';
-    div.style.borderRadius = '15px';
-    div.style.padding = '12px'
-    div.style.marginTop = '12px'
-    st.appendChild(div)
-    task = ""
+        const btnCreat = document.createElement('button');
+        btnCreat.textContent = "Done";
+        btnCreat.setAttribute('id','btnIn');
+        // btnCreat.classList.add('btnIn')
+        btnCreat.addEventListener('click',(e)=>{
+            console.log('Done btn clicked!');
+            // console.log(btnCreat.parentNode.parentNode.childNodes[0])
+            // console.log(typeof btnCreat.parentNode.parentNode.childNodes[0].textContent)
+            // console.log(record[record.indexOf(toString(btnCreat.parentNode.parentNode.childNodes[0]))]);
+            // console.log(record.indexOf(btnCreat.parentNode.parentNode.childNodes[0].textContent))
+
+            // record[record.indexOf(btnCreat.parentNode.parentNode.childNodes[0].textContent)] = ''
+            record.splice(record.indexOf(btnCreat.parentNode.parentNode.childNodes[0].textContent),1);
+            console.log(record)
+            btnCreat.parentNode.parentNode.remove();
+        })
+
+
+        const btnEdt = document.createElement('button');
+        btnEdt.textContent = "Edit"
+        btnEdt.setAttribute('id','btnEdt')
+
+        btnDiv.append(btnCreat);
+        btnDiv.append(btnEdt);
+        const div = document.createElement('div');
+        div.appendChild(document.createTextNode(task))
+        // div.appendChild(btnCreat);
+        // div.appendChild(btnEdt)
+        div.append(btnDiv)
+        div.setAttribute('id','taskDiv');
+
+        st.appendChild(div)
+        // task = ""
+        
     }
+    document.getElementById('taskIn').value = ''
 })
+
+
